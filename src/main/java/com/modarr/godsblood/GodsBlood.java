4a767cd0383cd318df0762b045b253bcb7eb8631
ModarrCode <modarrlock@gmail.com>
2025-07-07 18:14:53 +0100

    Added a separate creative mode tab for God's Blood and Blood block

--- src/main/java/com/modarr/godsblood/GodsBlood.java
+++ src/main/java/com/modarr/godsblood/GodsBlood.java
@@ -1,6 +1,7 @@
 package com.modarr.godsblood;
 
 import com.modarr.godsblood.block.ModBlocks;
+import com.modarr.godsblood.item.ModCreativeModeTabs;
 import com.modarr.godsblood.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -35,6 +36,8 @@ public class GodsBlood {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/com/modarr/godsblood/item/ModCreativeModeTabs.java
@@ -0,0 +1,30 @@
+package com.modarr.godsblood.item;
+
+import com.modarr.godsblood.GodsBlood;
+import com.modarr.godsblood.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GodsBlood.MODID);
+
+    public static final Supplier<CreativeModeTab> GODS_BLOOD_TAB = CREATIVE_MODE_TAB.register("gods_blood_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ITEMGODSBLOOD.get()))
+                    .title(Component.translatable("creativetab.godsblood.gods_blood"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                      output.accept(ModItems.ITEMGODSBLOOD);
+                      output.accept(ModBlocks.BLOOD_BLOCK);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+}
